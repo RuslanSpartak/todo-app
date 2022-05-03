5b33185ec787a83dcfaf3f2d8230d7858f3b0acc
RuslanSpartak <64797883+RuslanSpartak@users.noreply.github.com>
2022-05-03 20:21:52 +0200

    add useMemo for filtering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import Form from './components/Form'
 import TodoList from './components/TodoList';
 
@@ -8,44 +8,59 @@ function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
-  const [filteredTodos, setFilteredTodos] = useState([]);
+  // const [filteredTodos, setFilteredTodos] = useState([]);
+
+  const filteredTodos = useMemo( () => {
+    switch (status) {
+      case 'completed':
+        return (todos.filter(todo => todo.completed === true));
+      case 'uncompleted':
+        return (todos.filter(todo => todo.completed === false));
+      default:
+        return (todos);
+    }
+  });
 
   //RUN ONCE 
   useEffect (() => {
+
     getLocalTodos();
-  }, [] );
+  }, []);
 
   useEffect (() => {
-    filterHandler();
-    saveLocalTools();
+    // filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
-  const filterHandler = () => {
-    switch (status) {
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed === true));
-        break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed === false));
-        break;
-      default:
-        setFilteredTodos(todos);
-        break;
+  // const filterHandler = () => {
+  //   switch (status) {
+  //     case 'completed':
+  //       setFilteredTodos(todos.filter(todo => todo.completed === true));
+  //       break;
+  //     case 'uncompleted':
+  //       setFilteredTodos(todos.filter(todo => todo.completed === false));
+  //       break;
+  //     default:
+  //       setFilteredTodos(todos);
+  //       break;
+  //   }
+  // };
 
+
+  const saveLocalTodos = () => {
+    if (todos.length > 0) {
+      localStorage.setItem('todos', JSON.stringify(todos));
     }
-  };
-
-
-  const saveLocalTools = () => {
-    localStorage.setItem('todos', JSON.stringify(todos));
   };
 
   const getLocalTodos = () => {
     if (localStorage.getItem('todos') === null) {
       localStorage.setItem('todos', JSON.stringify([]));
     } else {
-      let localTodo = JSON.parse(localStorage.getItem('todos'));
-      setTodos(localTodo);
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      console.log(todoLocal);
+      setTodos(todoLocal);
+      // console.log(todos);
     }
   };
 
